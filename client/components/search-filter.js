373ffc9b03d1d547c2b2605750b6afa096ffbe89
veryspry <ehlinger.matt@gmail.com>
2018-07-25 16:14:39 -0500

    basic styling & layout in place

--- client/components/search-filter.js
+++ client/components/search-filter.js
@@ -2,11 +2,59 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchCategories } from '../store/filter'
 
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classnames from 'classnames';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import red from '@material-ui/core/colors/red';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
+
+const styles = theme => ({
+  card: {
+    maxWidth: 400,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  actions: {
+    display: 'flex',
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+    marginLeft: 'auto',
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+})
+
 class SearchFilter extends React.Component {
 
 
   state = {
-    categories: []
+    categories: [],
+    expanded: false,
   }
 
   async componentDidMount() {
@@ -20,8 +68,13 @@ class SearchFilter extends React.Component {
     })
   }
 
+  handleExpandClick = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  }
+
   render() {
-    // console.log('these are the props', this.props);
+
+    const { classes } = this.props;
 
       let categories = this.state.categories
 
@@ -29,39 +82,73 @@ class SearchFilter extends React.Component {
 
         return (
           <div>
-            <h1>This is a filter bar</h1>
-            {
-              // add categories
-              categories.map(category => {
-                console.log('hey Im in the map');
-                return (
-                  <div key={category.id}>
-                    <h3>{category.name}</h3>
+            {/* <h1>Filter Your Search</h1> */}
+            <Card className={classes.card}>
+              <CardHeader
+                avatar={
+                  <Avatar aria-label="" className={classes.avatar}>
+                    F
+                  </Avatar>
+                }
+                action={
+                  <IconButton>
+                    {/* <MoreVertIcon /> */}
+                  </IconButton>
+                }
+                title="Filter Your Search"
+              />
+                { // add categories
+                  categories.map(category => {
+                    return (
+                      <div key={category.id}>
 
-                    <form>
-                      {
-                        // add category values
-                        category.categoryvalues.map(val => {
-                          return (
-                            <div key={val.id}>
-                              <input type="checkbox" id={val.id} value={val.name}/>
-                              <label htmlFor={val.name}>val.name</label>
-                            </div>
-                          )
-                        })
-                      }
 
-                    </form>
-                  </div>
-                )
-              })
-            }
+                          <CardActions className={classes.actions} disableActionSpacing>
+                            <Typography>
+                              {category.name}
+                            </Typography>
+
+                            <IconButton
+                              className={classnames(classes.expand, {
+                                [classes.expandOpen]: this.state.expanded,
+                              })}
+                              onClick={this.handleExpandClick}
+                              aria-expanded={this.state.expanded}
+                              aria-label="Show more"
+                            >
+                              <ExpandMoreIcon />
+                            </IconButton>
+                          </CardActions>
+                          <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
+                            <CardContent>
+                              <div key={category.id}>
+                                <form>
+                                  {
+                                    // add category values
+                                    category.categoryvalues.map(val => {
+                                      return (
+                                        <div key={val.id}>
+                                          <input type="checkbox" id={val.id} value={val.name}/>
+                                          <label htmlFor={val.name}>{val.name}</label>
+                                        </div>
+                                      )
+                                    })
+                                  }
+                                </form>
+                              </div>
+                            </CardContent>
+                          </Collapse>
+
+                      </div>
+                    )}
+                )}
+              </Card>
           </div>
         )
-
   }
-
 }
+
+
 
 const mapStateToProps = state => ({
   categories: state.categories,
@@ -71,4 +158,4 @@ const mapDispatchToProps = dispatch => ({
   fetchCategories: () => dispatch(fetchCategories())
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(SearchFilter)
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(SearchFilter))
