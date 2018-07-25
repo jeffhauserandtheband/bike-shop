2950083c45ecc3245d682854012a78753ca61e81
Raymond Chao <raymond.chao16@gmail.com>
2018-07-25 15:33:15 -0500

    added basic single view

--- client/components/index.js
+++ client/components/index.js
@@ -6,4 +6,5 @@
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
 export {default as AllBikes} from './AllBikes'
+export {default as SingleBike} from './SingleBike'
 export {Login, Signup} from './auth-form'
--- client/store/bikes.js
+++ client/store/bikes.js
@@ -5,21 +5,22 @@ import history from '../history'
  * ACTION TYPES
  */
 const GET_BIKES = 'GET_BIKES'
-// const GET_SINGLE_BIKE = 'GET_SINGLE_BIKE'
+const GET_SINGLE_BIKE = 'GET_SINGLE_BIKE'
 // const REMOVE_USER = 'REMOVE_USER'
 
 /**
  * INITIAL STATE
  */
 const initialState = {
-    bikes: []
+    bikes: [],
+    singleBike: []
 }
 
 /**
  * ACTION CREATORS
  */
 const getBikes = bikes => ({type: GET_BIKES, bikes})
-
+const getOneBike = bike => ({type: GET_SINGLE_BIKE, bike})
 
 /**
  * THUNK CREATORS
@@ -38,6 +39,23 @@ const getBikes = bikes => ({type: GET_BIKES, bikes})
          dispatch(getBikes(res.data))
          history.push('/bikes')
      } catch (err) {
+         console.error(err)
+     }
+ }
+
+ export const fetchOneBike = (id) => async dispatch => {
+     let res
+     try {
+         res = await axios.get(`/api/bikes/${id}`)
+     } catch(err) {
+         //more pending error handling
+         return dispatch(getOneBike({error: err.message}))
+     }
+
+     try {
+         dispatch(getOneBike(res.data))
+         history.push(`/bikes/${id}`)
+     } catch(err) {
          console.error(err)
      }
  }
@@ -67,9 +85,8 @@ export default function(state = initialState, action) {
   switch (action.type) {
     case GET_BIKES:
         return {...state, bikes: action.bikes}
-    //   return action.bikes
-    // case REMOVE_USER:
-    //   return defaultUser
+    case GET_SINGLE_BIKE:
+        return {...state, singleBike: action.bike}
     default:
       return state
   }
