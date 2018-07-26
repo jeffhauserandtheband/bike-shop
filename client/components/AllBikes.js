523072a8258c09951e38a28eb63278b2d69fff32
Jessica Smith <smith.j.m.13@gmail.com>
2018-07-26 11:24:52 -0500

    random styling and functionality tweaks

--- client/components/AllBikes.js
+++ client/components/AllBikes.js
@@ -1,64 +1,98 @@
 import React, {Component} from 'react'
-import {Grid, Paper} from '@material-ui/core'
-import { connect } from 'react-redux';
+import PropTypes from 'prop-types'
+import {withStyles} from '@material-ui/core/styles'
+import {
+  Grid,
+  Card,
+  CardActions,
+  CardMedia,
+  CardContent,
+  Button,
+  Typography
+} from '@material-ui/core'
+import {connect} from 'react-redux'
 import {fetchBikes} from '../store'
 import {Link} from 'react-router-dom'
 
-const style = {
-    Paper: {
-        padding: 10,
-        marginTop:10,
-        marginBottom:10
-    }
-}
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  card: {
+    width: 300,
+    padding: theme.spacing.unit * 2
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+    width: 250,
+  }
+})
 
- class AllBikes extends Component {
-    componentDidMount() {
-        this.props.fetchBikes()
+class AllBikes extends Component {
+  componentDidMount() {
+    this.props.fetchBikes()
+  }
+
+  render() {
+    const {classes} = this.props
+
+    if (this.props.bikes.length === 0) {
+      return <Grid container>Loading..</Grid>
     }
 
-    render() {
-        
-        if (this.props.bikes.length === 0) {
+    return (
+      <div className={classes.root}>
+        <Grid container>
+          {this.props.bikes.map(elem => {
             return (
-                <Grid container>
-                    Loading..
-                </Grid>
+              <Grid spacing={24} key={elem.id}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    component={Link}
+                    to={`/bikes/${elem.id}`}
+                    className={classes.media}
+                    image={elem.bikeimages[0] && elem.bikeimages[0].imageUrl}
+                    title={elem.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="subheading" component="h3">
+                      <Link to={`/bikes/${elem.id}`}>{elem.name.substring()}</Link>
+                    </Typography>
+                  </CardContent>
+
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Add to cart
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
             )
-        }
+          })}
+        </Grid>
+      </div>
+    )
+  }
+}
+AllBikes.propTypes = {
+  classes: PropTypes.object.isRequired
+}
 
-        return(
-            <Grid container>
-            {this.props.bikes.map(elem => {
-                    return (
-                            <Grid item sm={2} key={elem.id}>
-                                <Link to={`/bikes/${elem.id}`}>
-                                <Paper style={style.Paper}>
-                                    
-                                    <img src={elem.bikeimages[0] && elem.bikeimages[0].imageUrl}/>
-                                    {elem.name}
-
-                                </Paper>
-                                </Link>
-                            </Grid>
-                    )
-                }
-            )}
-            </Grid>
-        )
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchBikes: () => {
+      dispatch(fetchBikes())
     }
+  }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {fetchBikes: () => {
-        dispatch(fetchBikes())
-    }}
+const mapStateToProps = state => {
+  return {
+    bikes: state.bikes.bikes
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        bikes: state.bikes.bikes
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(AllBikes)
+export default connect(mapStateToProps, mapDispatchToProps)(
+  withStyles(styles)(AllBikes)
+)
