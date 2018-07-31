d36ad0efe60345d2253476076a590b79254059ef
veryspry <ehlinger.matt@gmail.com>
2018-07-31 11:20:10 -0500

    add more users, photos and orders

--- script/seed.js
+++ script/seed.js
@@ -15,6 +15,9 @@ const {
 // library to generate fake data
 const faker = require('faker')
 
+const orders = require('./data/orders')
+const users = require('./data/users')
+
 // Some bike brands
 const bikeBrands = [
   'Schwinn',
@@ -39,30 +42,18 @@ const useCaseList = [
   'commuter'
 ]
 
-//order entries
-const orders = [
-  {
-    userId: 1,
-    state: 'completed',
-    shippingEmail: 'cody@email.com',
-    shippingName: 'cody jones',
-    shippingAddress: '123 Meadow Lane',
-    shippingCity: 'Chicago',
-    shippingState: 'IL',
-    shippingZip: 60606,
-    orderCost: 200
-  },
-  {
-    userId: 2,
-    state: 'processing',
-    shippingEmail: 'murphy@email.com',
-    shippingName: 'murphy jones',
-    shippingAddress: '125453 Butter Lane',
-    shippingCity: 'Chicago',
-    shippingState: 'IL',
-    shippingZip: 60601,
-    orderCost: 200
-  }
+const imageUrls = [
+  'https://images.unsplash.com/photo-1485965120184-e220f721d03e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=51a9aa4dd828bf5d50fcd8154dc405b8&auto=format&fit=crop&w=1050&q=80',
+  'https://images.unsplash.com/photo-1507035895480-2b3156c31fc8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b104c850fee7b7c4035e1477b1c13704&auto=format&fit=crop&w=1350&q=80',
+  'https://images.unsplash.com/photo-1505705694340-019e1e335916?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=c153251d0a63e31e85c6c818f7874dd2&auto=format&fit=crop&w=1189&q=80',
+  'https://images.unsplash.com/photo-1511994298241-608e28f14fde?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=735dff7662fcaf93e202d7a1aa76c313&auto=format&fit=crop&w=1050&q=80',
+  'https://images.unsplash.com/photo-1501236570302-906143a7c9f8?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=560e0a540d582b6127db8b21b1363df2&auto=format&fit=crop&w=967&q=80',
+  'https://images.unsplash.com/photo-1506567859980-33c9c423649f?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a3b98896ec1850fefd1fad9c763a935d&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1470855590377-4363884be8a3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a249adf93e39a03b3fd9b27d3705e185&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1463780924135-dc9b4d15fb87?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=cd18c25e45f96364dfdd503fbd78bb52&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1466838931486-92f3b5ff31a6?ixlib=rb-0.3.5&s=beda27df1dcebf394db8df029c7b6322&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1484144709249-a643e3720d13?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=09a47f6cde9a3102a94b4e7d5b90b420&auto=format&fit=crop&w=500&q=60',
+  'https://images.unsplash.com/photo-1508772382388-0b8fc7f7edb5?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=4b270517beb964c3e6817ba19f565406&auto=format&fit=crop&w=500&q=60'
 ]
 
 
@@ -72,26 +63,22 @@ function getRandomInt(max) {
 }
 
 async function seed() {
-  // await db.sync({force: true})
-  // console.log('db synced!')
-
-  // const users = await Promise.all([
-  //   // make some users
-  //   User.create({email: 'cody@email.com', password: '123'}),
-  //   User.create({email: 'greg@email.com', password: '123'}),
-  //   User.create({email: 'matt@gmail.com', password: 'test', userType: 'admin'})
-  // ])
 
   try {
     await db.sync({force: true})
     console.log('db synced!')
 
-    const users = await Promise.all([
-      // make some users
-      User.create({email: 'cody@email.com', password: '123'}),
-      User.create({email: 'murphy@email.com', password: '123'}),
-      User.create({email: 'matt@gmail.com', password: 'test', userType: 'admin'})
-    ])
+    // const users = await Promise.all([
+    //   // make some users
+    //   User.create({email: 'cody@email.com', password: '123'}),
+    //   User.create({email: 'murphy@email.com', password: '123'}),
+    //   User.create({email: 'matt@gmail.com', password: 'test', userType: 'admin'})
+    // ])
+
+    // create some users
+    await Promise.all(users.map(user => {
+      User.create(user)
+    }))
     // set up the CategoryKey's
     const brand = await CategoryKey.create({name: 'brand'}) // id: 1
     const color = await CategoryKey.create({name: 'color'}) // id: 2
@@ -142,13 +129,15 @@ async function seed() {
         availability: 'available' // enum: 'available', 'discontinued'
       })
 
-      // add a brand
+      // add a brand to the bike
       const addedBrand = await bike.addCategoryvalue(
         brands[getRandomInt(brands.length - 1)].id
       )
+      // add a color to the bike
       const addedColors = await bike.addCategoryvalue(
         colors[getRandomInt(colors.length - 1)].id
       )
+      // add a use case to the bike
       const addedUseCases = await bike.addCategoryvalue(
         useCases[getRandomInt(useCases.length - 1)].id
       )
@@ -157,16 +146,19 @@ async function seed() {
       for (let j = 0; j < getRandomInt(7); j++) {
         await BikeImage.create({
           bikeId: bike.id,
-          imageUrl: 'http://learndotresources.s3.amazonaws.com/workshop/55e5c92fe859dc0300619bc8/sloth.jpg'
-            
+          imageUrl: imageUrls[getRandomInt(imageUrls.length-1)],
         })
       }
     }
 
-    await Promise.all(orders.map(order => Order.create(order)))
+    // Create orders
+    await Promise.all(orders.map(order => {
+      Order.create(order)
+    }))
     const bikes = await Bike.findAll()
     const createdOrders = await Order.findAll()
 
+    // add a bike to each order
     await Promise.all(
       createdOrders.map(order => {
         const bike = bikes[getRandomInt(bikes.length - 1)]
@@ -174,6 +166,7 @@ async function seed() {
       })
     )
 
+    // make order entries
     const orderEntries = await OrderEntries.findAll()
     await Promise.all(
       orderEntries.map(orderEntry => {
