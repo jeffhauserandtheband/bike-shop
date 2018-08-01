80a954bb59990889593df3d6f28badf392338d4c
Jessica Smith <smith.j.m.13@gmail.com>
2018-08-01 09:40:26 -0500

    fixes

--- client/components/admin/Categories.js
+++ client/components/admin/Categories.js
@@ -1,83 +1,60 @@
-import React, {Component} from 'react'
+import React, {Fragment} from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {logout} from '../../store'
 import {withStyles} from '@material-ui/core/styles'
 import {
   Paper,
-  Typography,
   Table,
   TableBody,
   TableCell,
   TableHead,
-  TableRow
+  TableRow,
+  Button
 } from '@material-ui/core'
-
-//import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 
 const styles = {
   root: {
     flexGrow: 1,
     width: 200,
+    flexDirection: 'column'
   },
-  
+  container: {
+    display: 'flex',
+    justifyContent: 'center'
+  }
 }
 
-class Categories extends Component { 
-  render() {
-    const {categories, classes} = this.props
-    return (
-      <div>
-      {categories.map(category=> (
+const Categories = ({categories, classes}) => (
+  <Fragment>
+    <Button component={Link} to="/admin/addcategory" type="submit" color="secondary" variant="contained">
+      Add Category
+    </Button>
+    <div className={classes.container}>
+      {categories.map(category => (
         <Paper className={classes.root} key={category.id}>
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>{category.name}</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {category.categoryvalues.map(value => {
-              return (
-                <TableRow key={value.id}>
-                  <TableCell>{value.name}</TableCell>
-                </TableRow>
-              )
-            })}
-          </TableBody>
-        </Table>
-      </Paper>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>{category.name}</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {category.categoryvalues.map(value => {
+                return (
+                  <TableRow key={value.id}>
+                    <TableCell>{value.name}</TableCell>
+                  </TableRow>
+                )
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
       ))}
+    </div>
+  </Fragment>
+)
 
-      <Link>
-        <button type='submit'> Add Category </button>
-      </Link>
-      </div>
-    )
-  }
-  
-
-  
-
-
-}
-
-/**
- * CONTAINER
- */
-
-const mapState = state => {}
-
-const mapDispatch = dispatch => {
-  return {
-    // postCategoryKey: () => {
-    //   dispatch(postCategoryKey())
-    // }
-  }
-}
-
-export default connect(mapState, mapDispatch)(withStyles(styles)(Categories))
+export default withStyles(styles)(Categories)
 
 /**
  * PROP TYPES
